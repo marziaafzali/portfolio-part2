11478898cdd46952c0c43a8347b576a48c537a83
marziaafzali <afzalimarzia2004@gmail.com>
2025-12-20 22:36:36 +0430

    commit

--- src/component/Aside.jsx
+++ src/component/Aside.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { FaQuoteLeft, FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Aside() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -9,14 +9,17 @@ function Aside() {
     { path: "/", icon: "fa fa-home", label: "Home", exact: true },
     { path: "/about", icon: "fa fa-user", label: "About" },
     { path: "/projects", icon: "fa fa-briefcase", label: "Projects" },
-    { path: "/sertificates", icon: "fas fa-certificate", label: "Certificate" },
-   // { path: "/feedback", icon: <FaQuoteLeft style={{  marginRight: "10px" }} />, label: "Testimonial" },
+    {
+      path: "/sertificates",
+      icon: "fas fa-certificate",
+      label: "Certificate",
+    },
     { path: "/contact", icon: "fa fa-comments", label: "Contact" },
   ];
 
   return (
     <>
-      {/* Hamburger button (visible on small screens) */}
+      {/* Hamburger button (small screens) */}
       <div className="nav-toggler" onClick={() => setMenuOpen(!menuOpen)}>
         {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
@@ -30,7 +33,6 @@ function Aside() {
         </div>
 
         <ul className="nav">
-<<<<<<< Updated upstream
           {navItems.map((item, index) => (
             <li key={index}>
               <NavLink
@@ -41,23 +43,11 @@ function Aside() {
                   `nav-link ${isActive ? "active" : ""}`
                 }
               >
-                {typeof item.icon === "string" ? (
-                  <i className={item.icon}></i>
-                ) : (
-                  item.icon
-                )}
+                <i className={item.icon}></i>
                 <span className="nav-label">{item.label}</span>
               </NavLink>
             </li>
           ))}
-=======
-          <li><Link  to="/" onClick={() => setMenuOpen(false)}><span><i className="fa fa-home"></i> Home </span></Link></li>
-          <li><Link to="/about" onClick={() => setMenuOpen(false)}><i className="fa fa-user"></i> About</Link></li>
-          <li><Link to="/projects" onClick={() => setMenuOpen(false)}><i className="fa fa-briefcase"></i> Projects</Link></li>
-          <li><Link to="/sertificates" onClick={() => setMenuOpen(false)}><i className="fas fa-certificate"></i> Sertificate</Link></li>
-          {/*<li><Link to="/feedback" onClick={() => setMenuOpen(false)}><FaQuoteLeft style={{ marginRight: "8px" }} /> Testimonial</Link></li>*/}
-          <li><Link to="/contact" onClick={() => setMenuOpen(false)}><i className="fa fa-comments"></i> Contact</Link></li>
->>>>>>> Stashed changes
         </ul>
       </aside>
     </>
